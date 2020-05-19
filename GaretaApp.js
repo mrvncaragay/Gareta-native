5b74c470cad9470417492b56dcaf24fcfce021f1
Marvin Caragay <mrvncaragay@gmail.com>
2020-05-19 01:06:37 -0700

    Add styled components and Basic navigation

--- GaretaApp.js
+++ GaretaApp.js
@@ -1,19 +1,53 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { View, Text } from "react-native";
+import styled from "styled-components";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
+import { NavigationContainer } from "@react-navigation/native";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+
+const Profile = styled.Text`
+  font-family: 'SourceSerifPro-Bold';
+  color: red
+`;
+
+const Save = styled.Text`
+  font-family: 'SourceSerifPro-Bold';
+  color: red
+`;
+
+const Cool = styled.Text`
+  font-family: 'SourceSerifPro-Bold';
+  color: red
+`;
+
+const Featured = styled.Text`
+  font-family: 'SourceSerifPro-Bold';
+  color: red
+`;
+
+const BottomTab = createMaterialBottomTabNavigator();
+const TopTab = createMaterialTopTabNavigator();
 
 export default function App() {
+  const createHomeTopTab = () => (
+    <TopTab.Navigator tabBarOptions={{ indicatorStyle: { backgroundColor: "red" } }}>
+      <TopTab.Screen name="Featured" component={Featured} />
+      <TopTab.Screen name="Categories" component={Featured} />
+    </TopTab.Navigator>
+  );
+
   return (
-    <View style={styles.container}>
-      <Text style={{ fontFamily: "SourceSerifPro-Bold" }}>Open up App.js to start working on your app! 4</Text>
-    </View>
+    <NavigationContainer>
+      <View style={{ height: 70, backgroundColor: "#374e62" }}>
+        <Text>This is a head</Text>
+      </View>
+      <BottomTab.Navigator activeColor="black" barStyle={{ backgroundColor: "#fff" }}>
+        <BottomTab.Screen name="Home" children={createHomeTopTab} />
+        <BottomTab.Screen name="Profile" component={Profile} />
+        <BottomTab.Screen name="Save" component={Save} />
+        <BottomTab.Screen name="Cool" component={Cool} />
+      </BottomTab.Navigator>
+    </NavigationContainer>
   );
 }
